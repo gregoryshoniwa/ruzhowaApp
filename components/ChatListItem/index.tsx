c8ea83dd528452e1759df6ecb335aa1e0b4a9319
Gregory Shoniwa <Nmbbankzim@gmail.com>
2021-05-03 13:03:25 +0200

    chat component

--- components/ChatListItem/index.tsx
+++ components/ChatListItem/index.tsx
@@ -1,15 +1,32 @@
 import React from 'react';
-import { View,Text } from 'react-native';
+import { View,Text,Image } from 'react-native';
 import { ChatRoom } from '../../types';
+import styles from './style'
 
 export type ChatListItemProps = {
     chatRoom:ChatRoom;
 }
+
+
 const ChatListItem = (props:ChatListItemProps) => {
-    const { chatRoom } = props
-    return(
-        <View>
-            <Text>{chatRoom.lastMessage.content}</Text>
+    const { chatRoom } = props;
+    const user = chatRoom.users[0];
+    return( 
+        <View style={styles.constainer} >
+            <View style={styles.leftContainer}>
+
+                <Image source={{ uri : user.imageUri}} style={styles.avatar} />
+                
+                <View style={styles.midContainer}>
+                    <Text style={styles.username}>{user.name}</Text>
+                    <Text style={styles.lastMessage}>{chatRoom.lastMessage.content}</Text>
+                </View>
+
+            </View>
+            
+            
+            {/* <Text>{chatRoom.lastMessage.createdAt}</Text> */}
+            <Text style={styles.time}>Yesterday</Text>
         </View>
     )
 }
